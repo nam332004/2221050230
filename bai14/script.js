fc85b079f41fb04c158e0cf30f2778134ee6efcb
nam332004 <hoangnam332004>
2025-11-03 18:20:57 +0700

    buổi 14

---
+++ bai14/script.js
@@ -0,0 +1,32 @@
+let danSachPhim = [
+    {
+    id:1,
+    tenPhim:"mưa đỏ",
+    namPhatHanh: 2025,
+    tuoi:16,
+    thoiLuong:2,
+    quocGia:"Việt Nam",
+    poster: 
+    theLoai : "phim chiếu rạp",
+    },
+
+    {
+    id:2,
+    tenPhim:"conan",
+    namPhatHanh: 2023,
+    tuoi:10,
+    thoiLuong:1.5,
+    quocGia:"Nhật Bản",
+    poster:
+    theLoai : "phim chiếu rạp",
+    
+}
+];
+function chonphim(idphim){
+let phimHienTai = danhSachPhim[0];
+  for (let i=0; i< danhSachPhim.length;i++) {
+    if (danhSachPhim[i].id == idPhim){
+        banner.style.backgroundImange = danhSachPhim[i].poster
+    }
+  }
+}
